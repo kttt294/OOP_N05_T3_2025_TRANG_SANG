adf33610ebdc6d526a350cd23c37fbe3098951cc
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-06-24 11:24:41 +0000

    trnag commit

--- CinemaManager/test/BreakAndContinue.java
+++ CinemaManager/test/BreakAndContinue.java
@@ -1,9 +1,29 @@
 public class BreakAndContinue {
-    public static void test(){
+    public static void testFor(){
         for(int i=0; i<100; i++){
             if(i == 74) break;
             if(i%9 != 0) continue;
             System.out.println(i);
         }
+    }
+
+    public static void testWhile(){
+        int i = 0;
+        while(i<100){
+            if(i == 74) break;
+            if(i%9 != 0) continue;
+            System.out.println(i);
+            i++;
+        }
+    }
+
+    public static void testDoWhile(){
+        int i=0;
+        do{
+            if(i == 74) break;
+            if(i%9 != 0) continue;
+            System.out.println(i);
+            i++;
+        } while(i<100);
     }
 }
