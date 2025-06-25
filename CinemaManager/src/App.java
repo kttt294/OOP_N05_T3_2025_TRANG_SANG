73924f8bfc46f77e0e893e280c22672831a6b134
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-06-25 07:24:09 +0000

    trang commit

--- CinemaManager/src/App.java
+++ CinemaManager/src/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        //testNumber.test();
-        testPassObject.test1();
-        BreakAndContinue.testFor();
-        BreakAndContinue.testWhile();
-        BreakAndContinue.testDoWhile();
+                
     }
 }
