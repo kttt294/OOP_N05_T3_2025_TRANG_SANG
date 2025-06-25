017489fe50aad3be2f9987bbcf32e9865284eca4
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-06-25 08:46:20 +0000

    trang commit

--- CinemaManager/src/App.java
+++ CinemaManager/src/App.java
@@ -5,7 +5,9 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
             testKhachHang.test(scanner);
+            System.out.println();
             testPhim.test(scanner);
+            System.out.println();
             testSuatChieu.test(scanner);
     }
 }
