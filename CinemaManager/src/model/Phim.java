e3c7b417d01d08a18787f5feacef1b30ef2fddaf
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-06-25 08:06:45 +0000

    trang commit

--- CinemaManager/src/model/Phim.java
+++ CinemaManager/src/model/Phim.java
@@ -1,6 +1,6 @@
 package model;
 
-public class Phim{
+public class Phim {
     String maPhim;
     String tenPhim;
     String theLoai;
@@ -8,5 +8,61 @@ public class Phim{
     String ngonNgu;
     int gioiHanTuoi;
     String moTa;
-    DanhGia danhGia;
+
+    public Phim(String maPhim, String tenPhim, String theLoai, int thoiLuong,
+                String ngonNgu, int gioiHanTuoi, String moTa) {
+        this.maPhim = maPhim;
+        this.tenPhim = tenPhim;
+        this.theLoai = theLoai;
+        this.thoiLuong = thoiLuong;
+        this.ngonNgu = ngonNgu;
+        this.gioiHanTuoi = gioiHanTuoi;
+        this.moTa = moTa;
+    }
+
+    public String getMaPhim() {
+        return maPhim;
+    }
+
+    public void setMaPhim(String maPhim) {
+        this.maPhim = maPhim;
+    }
+
+    public String getTenPhim() {
+        return tenPhim;
+    }
+
+    public void setTenPhim(String tenPhim) {
+        this.tenPhim = tenPhim;
+    }
+
+    public int getThoiLuong() {
+        return thoiLuong;
+    }
+
+    public void setThoiLuong(int thoiLuong) {
+        this.thoiLuong = thoiLuong;
+    }
+
+    public int getGioiHanTuoi() {
+        return gioiHanTuoi;
+    }
+
+    public void setGioiHanTuoi(int gioiHanTuoi) {
+        this.gioiHanTuoi = gioiHanTuoi;
+    }
+
+    public void hienThiThongTin() {
+        System.out.println("Mã phim: " + maPhim);
+        System.out.println("Tên phim: " + tenPhim);
+        System.out.println("Thể loại: " + theLoai);
+        System.out.println("Thời lượng: " + thoiLuong + " phút");
+        System.out.println("Ngôn ngữ: " + ngonNgu);
+        System.out.println("Giới hạn tuổi: " + gioiHanTuoi + "+");
+        System.out.println("Mô tả: " + moTa);
+    }
+
+    public boolean kiemTraDoTuoi(int tuoiNguoiXem) {
+        return tuoiNguoiXem >= gioiHanTuoi;
+    }
 }
---
+++ CinemaManager/test/testPhim.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import model.Phim;
+
+public class testPhim {
+    public static void test(){
+        Scanner sc = new Scanner(System.in);
+        String maPhim, tenPhim, theLoai, ngonNgu, moTa;
+        int thoiLuong, gioiHanTuoi;
+        Phim phim = new Phim(maPhim, tenPhim, theLoai, thoiLuong, ngonNgu, gioiHanTuoi, moTa);
+    }
+}
