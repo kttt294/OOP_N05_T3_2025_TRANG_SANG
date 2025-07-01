b861e293c132a61a9956f1451c9bae3a96d10cd8
TranMinhSang <skksang2k6@gmail.com>
2025-07-01 09:42:25 +0000

    sangc15

--- CinemaManager/src/App.java
+++ CinemaManager/src/App.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+
+import review.Book;
 import test.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        testKhachHang.test(scanner);
-        testPhim.test(scanner);
-        testSuatChieu.test(scanner);
+        //testKhachHang.test(scanner);
+        //testPhim.test(scanner);
+        //testSuatChieu.test(scanner);
+        Book.test();
     }
 }
