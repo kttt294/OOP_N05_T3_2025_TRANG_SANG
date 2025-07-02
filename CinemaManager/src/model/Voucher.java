a3a444d435bf139facf16caf2268be22b3f957f8
TranMinhSang <skksang2k6@gmail.com>
2025-07-02 03:42:37 +0000

    sangc21

--- CinemaManager/src/model/Voucher.java
+++ CinemaManager/src/model/Voucher.java
@@ -81,26 +81,3 @@ public class Voucher {
         this.trangThai = trangThai;
     }
 }
-public class Voucher{
-    private String maVoucher;
-    private String moTa;
-    private int phanTramGiamGia;
-    private LocalDateTime ngayBatDau;
-    private LocalDateTime ngayKetThuc;
-    private boolean conHieuLuc;
-
-    public Voucher(){};
-    public Voucher(String maVoucher, String moTa, int phanTramGiamGia, LocalDateTime ngayBatDau, LocalDateTime ngayKetThuc, boolean conHieuLuc){
-        this.maVoucher = maVoucher;
-        this.moTa = moTa;
-
-    }
-
-    public void setMaVoucher(String ma){
-        this.maVoucher = ma;
-    }
-
-    public String getMaVoucher(){
-        return this.maVoucher;
-    }
-}
