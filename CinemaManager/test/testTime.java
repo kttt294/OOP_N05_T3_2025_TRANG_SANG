3bb6481b214b473e91fd3b5871e0c64bf08f090d
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-07-02 07:59:37 +0000

    trang commit

--- CinemaManager/test/testTime.java
+++ CinemaManager/test/testTime.java
@@ -6,7 +6,7 @@ public class testTime {
         Time t1 = new Time(2, 7, 2025, 15, 40, 00);
         Time t2 = new Time(2, 7, 2025,13,54,20);
 
-        System.out.println("Thoi gian bat dau: " + t2);
-        System.out.println("Thoi gian ket thuc: " + t1);
+        System.out.println("Thời gian bắt đầu: " + t2);
+        System.out.println("Thời gian kết thúc: " + t1);
     }
 }
