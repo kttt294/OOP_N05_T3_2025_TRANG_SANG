e9a2e608c863890a52c0b86b146af9fde467bf25
TranMinhSang <skksang2k6@gmail.com>
2025-07-02 06:58:14 +0000

    sangc22

---
+++ CinemaManager/review/testTime.java
@@ -0,0 +1,9 @@
+public class testTime {
+    public static void test(){
+        Time t1 = new Time();
+        Time t2 = new Time(13,54,20);
+
+        System.out.println("Thoi gian mac dinh" + t1);
+        System.out.println("Thoi gian thuc:" + t2);
+    }
+}
