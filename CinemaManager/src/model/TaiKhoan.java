bc4bc9fffc12a18bb1ccf6dabde4e4cdaaf83032
sangzesy <skksang2k6@gmail.com>
2025-07-05 15:45:24 +0700

    sangc24

--- CinemaManager/src/model/TaiKhoan.java
+++ CinemaManager/src/model/TaiKhoan.java
@@ -4,15 +4,47 @@ public class TaiKhoan {
     private String tenDangNhap;
     private String matKhau;
     private VaiTro vaiTro;
-    public enum VaiTro {ADMIN, NHANVIEN, KHACHHANG};
+    public enum VaiTro { ADMIN, NHANVIEN, KHACHHANG };
     private boolean trangThai; // true: hoạt động, false: bị khóa
 
-    public TaiKhoan() {};
+    public TaiKhoan() {}
 
     public TaiKhoan(String tenDangNhap, String matKhau, VaiTro vaiTro) {
         this.tenDangNhap = tenDangNhap;
         this.matKhau = matKhau;
         this.vaiTro = vaiTro;
         this.trangThai = true;
+    }
+
+    public String getTenDangNhap() {
+        return tenDangNhap;
+    }
+
+    public void setTenDangNhap(String tenDangNhap) {
+        this.tenDangNhap = tenDangNhap;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public VaiTro getVaiTro() {
+        return vaiTro;
+    }
+
+    public void setVaiTro(VaiTro vaiTro) {
+        this.vaiTro = vaiTro;
+    }
+
+    public boolean isTrangThai() {
+        return trangThai;
+    }
+
+    public void setTrangThai(boolean trangThai) {
+        this.trangThai = trangThai;
     }
 }
