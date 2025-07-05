5a01c07ddcbbf7d87b36f9385383e74c1789678f
sangzesy <skksang2k6@gmail.com>
2025-07-05 15:49:33 +0700

    sang25

--- CinemaManager/src/model/DanhGia.java
+++ CinemaManager/src/model/DanhGia.java
@@ -2,17 +2,19 @@ package model;
 
 import java.time.LocalDateTime;
 
-public class DanhGia{
+public class DanhGia {
     private String maDanhGia;
     private KhachHang nguoiDanhGia;
     private Phim phim;
-    private int soSao;   // từ 0 đến 5
+    private int soSao; // từ 0 đến 5
     private String binhLuan;
     private LocalDateTime thoiGian;
 
-    public DanhGia(){};
+    public DanhGia() {
+    };
 
-    public DanhGia(String maDanhGia, KhachHang nguoiDanhGia, Phim phim, int soSao, String binhLuan, LocalDateTime thoiGian){
+    public DanhGia(String maDanhGia, KhachHang nguoiDanhGia, Phim phim, int soSao, String binhLuan,
+            LocalDateTime thoiGian) {
         this.maDanhGia = maDanhGia;
         this.nguoiDanhGia = nguoiDanhGia;
         this.phim = phim;
@@ -20,4 +22,53 @@ public class DanhGia{
         this.binhLuan = binhLuan;
         this.thoiGian = thoiGian;
     }
+
+    public String getMaDanhGia() {
+        return maDanhGia;
+    }
+
+    public void setMaDanhGia(String maDanhGia) {
+        this.maDanhGia = maDanhGia;
+    }
+
+    public KhachHang getNguoiDanhGia() {
+        return nguoiDanhGia;
+    }
+
+    public void setNguoiDanhGia(KhachHang nguoiDanhGia) {
+        this.nguoiDanhGia = nguoiDanhGia;
+    }
+
+    public Phim getPhim() {
+        return phim;
+    }
+
+    public void setPhim(Phim phim) {
+        this.phim = phim;
+    }
+
+    public int getSoSao() {
+        return soSao;
+    }
+
+    public void setSoSao(int soSao) {
+        this.soSao = soSao;
+    }
+
+    public String getBinhLuan() {
+        return binhLuan;
+    }
+
+    public void setBinhLuan(String binhLuan) {
+        this.binhLuan = binhLuan;
+    }
+
+    public LocalDateTime getThoiGian() {
+        return thoiGian;
+    }
+
+    public void setThoiGian(LocalDateTime thoiGian) {
+        this.thoiGian = thoiGian;
+    }
+
 }
