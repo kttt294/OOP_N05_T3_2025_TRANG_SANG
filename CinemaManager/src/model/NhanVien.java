24ee50d499ce1265c17b615b01e3b9c957243ede
sangzesy <skksang2k6@gmail.com>
2025-07-05 15:42:36 +0700

    sangc23

--- CinemaManager/src/model/NhanVien.java
+++ CinemaManager/src/model/NhanVien.java
@@ -1,6 +1,6 @@
 package model;
 
-public class NhanVien{
+public class NhanVien {
     private String maNV;
     private String tenNV;
     private int tuoi;
@@ -10,5 +10,82 @@ public class NhanVien{
     private String chucVu;
     private TaiKhoan taiKhoan;
 
-    public NhanVien(){};
+    public NhanVien() {}
+
+    public NhanVien(String maNV, String tenNV, int tuoi, String sdt, String email,
+                    String gioiTinh, String chucVu, TaiKhoan taiKhoan) {
+        this.maNV = maNV;
+        this.tenNV = tenNV;
+        this.tuoi = tuoi;
+        this.sdt = sdt;
+        this.email = email;
+        this.gioiTinh = gioiTinh;
+        this.chucVu = chucVu;
+        this.taiKhoan = taiKhoan;
+    }
+
+    // Getter và Setter
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getTenNV() {
+        return tenNV;
+    }
+
+    public void setTenNV(String tenNV) {
+        this.tenNV = tenNV;
+    }
+
+    public int getTuoi() {
+        return tuoi;
+    }
+
+    public void setTuoi(int tuoi) {
+        this.tuoi = tuoi;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGioiTinh() {
+        return gioiTinh;
+    }
+
+    public void setGioiTinh(String gioiTinh) {
+        this.gioiTinh = gioiTinh;
+    }
+
+    public String getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(String chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public TaiKhoan getTaiKhoan() {
+        return taiKhoan;
+    }
+
+    public void setTaiKhoan(TaiKhoan taiKhoan) {
+        this.taiKhoan = taiKhoan;
+    }
 }
