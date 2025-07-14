b864fecc4e47408812d737109bb8d502051c907b
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-07-14 08:47:24 +0000

    trang commit

--- CinemaManager/src/App.java
+++ CinemaManager/src/App.java
@@ -1,24 +1,5 @@
 public class App {
     public static void main(String[] args){
-        // gọi test
-        /* 
-        btvn.AccessControl.ex1.testEx1.test();
-        btvn.AccessControl.ex3.testEx3_debug.test();
-        btvn.AccessControl.ex3.testEx3_debugoff.test();
-        btvn.AccessControl.ex5.testEx5.test();
-        btvn.AccessControl.ex6.testEx6.test();
-        btvn.AccessControl.ex8.testEx8.test();
-        btvn.AccessControl.ex9.testEx9.test();
-        btvn.AccessControl.ex10.testEx10.test();
-        btvn.InitialisationAndCleanup.ex1.testEx1.test();
-        btvn.InitialisationAndCleanup.ex2.testEx2.test();
-        btvn.InitialisationAndCleanup.ex3.testEx3.test();
-        btvn.InitialisationAndCleanup.ex4.testEx4.test();
-        btvn.InitialisationAndCleanup.ex5.testEx5.test();
-        */
-
-       // testCollection.test();
-        testTelList.test();
-
+        
     }
 }
