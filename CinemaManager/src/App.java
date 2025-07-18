1caab4fcfe1868f04593d7b725a291522defacdf
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-07-18 07:23:47 +0000

    trang commit

--- CinemaManager/src/App.java
+++ CinemaManager/src/App.java
@@ -2,6 +2,6 @@ import javax.swing.SwingUtilities;
 
 public class App {
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true))
+        SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
     }
 }
