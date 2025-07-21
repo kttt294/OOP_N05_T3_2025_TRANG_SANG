5afe87171763d4d2d7ab65682e8972f5d86c4c59
sangzesy <skksang2k6@gmail.com>
2025-07-22 05:11:05 +0700

    sangcmt

--- CinemaManager/test/testSuatChieuController.java
+++ CinemaManager/test/testSuatChieuController.java
@@ -1,13 +1,27 @@
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 
 public class testSuatChieuController {
-    public static void main(String[] args) {
-        SuatChieu sc1 = new SuatChieu("SC01", "P01", "PH01", LocalDateTime.of(2025, 7, 22, 14, 0),LocalDateTime.of(2025, 7, 22, 16, 0));
-        SuatChieu sc2 = new SuatChieu(
-        SuatChieu sc3 = new SuatChieu(
+    public static void test() {
+        Ghe g1 = new Ghe("G01", 1, 1, Ghe.LoaiGhe.THUONG, "PH01");
+        Ghe g2 = new Ghe("G02", 1, 2, Ghe.LoaiGhe.VIP, "PH01");
+        Ghe g3 = new Ghe("G03", 2, 1, Ghe.LoaiGhe.COUPLE, "PH02");
+
+        List<Ghe> danhSachGhe = Arrays.asList(g1, g2, g3);
+
+        SuatChieu sc1 = new SuatChieu("SC01", "P01", "PH01",
+                LocalDateTime.of(2025, 7, 22, 14, 0),
+                LocalDateTime.of(2025, 7, 22, 16, 0),
+                danhSachGhe);
+        SuatChieu sc2 = new SuatChieu("SC02", "P02", "PH02",
+                LocalDateTime.of(2025, 7, 22, 18, 0),
+                LocalDateTime.of(2025, 7, 22, 20, 0),
+                danhSachGhe);
+        SuatChieu sc3 = new SuatChieu("SC03", "P01", "PH01",
+                LocalDateTime.of(2025, 7, 23, 14, 0),
+                LocalDateTime.of(2025, 7, 23, 16, 0),
+                danhSachGhe);
 
         List<SuatChieu> danhSach = Arrays.asList(sc1, sc2, sc3);
 
