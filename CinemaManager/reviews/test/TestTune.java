07edd319830fe41a315ea1033036d25f881172cd
TranMinhSang <skksang2k6@gmail.com>
2025-07-21 03:30:02 +0000

    sang fix conflict

--- CinemaManager/reviews/test/TestTune.java
+++ CinemaManager/reviews/test/TestTune.java
@@ -1,7 +1,8 @@
 public class TestTune {
     public static void test() {
         CellPhone noiseMarker = new CellPhone();
-        ObnoxiousTune ot = new ObnoxiousTune();
+        //ObnoxiousTune ot = new Tune();
+        Tune ot = new ObnoxiousTune();
         noiseMarker.ring(ot);
     }
 }
