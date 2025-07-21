29def704a8fd9881c6fc4bdf964b4aa01cedc9d9
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-07-21 03:23:34 +0000

    trang commit

--- CinemaManager/src/App.java
+++ CinemaManager/src/App.java
@@ -1,9 +1,5 @@
-import javax.swing.SwingUtilities;
-import view.MainFrame;
-
 public class App {
     public static void main(String[] args) {
-        //SwingUtilities.invokeLater(() -> new MainFrame().setVisible(true));
         TestTune.test();
     }
 }
