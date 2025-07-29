099fd05f7bcc20f7e3d33ff363258fa26e83f9a4
TranMinhSang <skksang2k6@gmail.com>
2025-07-29 11:01:46 +0000

    s cmt

--- CinemaManagementApplication/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ CinemaManagementApplication/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -11,6 +11,8 @@ public class GreetingController {
 	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
 		model.addAttribute("name", name);
+		Phim tp = new Phim("abc", "test");
+		
 		return "phimtest";
 	}
 
