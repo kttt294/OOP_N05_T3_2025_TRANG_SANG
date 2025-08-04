1b001b459d91962a8e6a01166c1ae9b79ad814b7
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-08-04 04:29:06 +0000

    trang commit

--- CinemaManagementApplication/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ CinemaManagementApplication/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,6 +1,5 @@
 package com.example.servingwebcontent;
 
-import com.example.servingwebcontent.model.Phim;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +8,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+	@GetMapping("/greeting")
 	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		Phim tp = new Phim("abc", "test");
-		System.out.println("Ma phim: " + tp.maPhim);
-		model.addAttribute("tp", tp.maPhim);
-		
-		return "phimtest";
+		model.addAttribute("name", name);
+		return "greeting";
 	}
-
-	
-
-	
 
 }
