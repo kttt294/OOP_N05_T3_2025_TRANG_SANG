a9af8fb55349825f5e604d2a341e2341428951a4
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-08-05 09:44:03 +0000

    trang commit

--- CinemaManagementApplication/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ CinemaManagementApplication/complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,17 +1,38 @@
 package com.example.servingwebcontent;
 
+import com.example.servingwebcontent.model.KhachHang;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @Controller
+@RequestMapping("/khachhang")
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
+    // Hiển thị form thêm khách hàng
+    @GetMapping("/them")
+    public String hienThiForm(Model model) {
+        model.addAttribute("khachHang", new KhachHang());
+        return "form-khachhang"; // trả về file form-khachhang.html
+    }
 
+    // 
+    @PostMapping("/luu")
+    public String luuKhachHang(@ModelAttribute KhachHang khachHang, Model model) {
+        KhachHang.Create(khachHang);
+        ArrayList<KhachHang> danhSach = KhachHang.Read();
+        model.addAttribute("dsKH", danhSach);
+        return "khachhang";
+}
+
+
+    // Hiển thị danh sách khách hàng
+    @GetMapping("/danhsach")
+    public String hienThiDanhSach(Model model) {
+        ArrayList<KhachHang> danhSach = KhachHang.Read();
+        model.addAttribute("dsKH", danhSach); // trùng với th:each trong khachhang.html
+        return "khachhang";
+    }
 }
