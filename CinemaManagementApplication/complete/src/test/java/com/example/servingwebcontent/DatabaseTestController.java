dfba43d77b94bcb946e07ba1f4eea6460fdcf574
Kiều Thị Thu Trang <kieuthutrang2904@gmail.com>
2025-08-11 15:31:15 +0000

    trang commit

--- CinemaManagementApplication/complete/src/test/java/com/example/servingwebcontent/DatabaseTestController.java
+++ CinemaManagementApplication/complete/src/test/java/com/example/servingwebcontent/DatabaseTestController.java
@@ -1,4 +1,4 @@
-package com.example.servingwebcontent.controller;
+package com.example.servingwebcontent;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
